b61dafcadb922d9e9637c5f0f189bb0067938a39
genuine_code <113706866+genuinecode1@users.noreply.github.com>
2024-03-12 20:19:31 +0530

    Two-Pointers

---
+++ 125. Valid Palindrome.cpp
@@ -0,0 +1,34 @@
+
+
+/*
+
+Author :- Aditya Mani Tripathi
+leetcode username - genuine_code
+email - aadiitripathii46@gmail.com
+institute - UIET CSJM UNIVERSITY KANPUR
+
+*/
+
+
+
+// code :-
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size(), l = 0, r = n - 1;
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) {
+                l++;
+            }
+            while (l < r && !isalnum(s[r])) {
+                r--;
+            }
+            if (tolower(s[l++]) != tolower(s[r--])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
