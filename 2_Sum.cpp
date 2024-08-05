e296cfda828734fc59d3186aad9783a70c758dc3
genuine_code <113706866+genuinecode1@users.noreply.github.com>
2024-08-05 21:14:44 +0530

    2Sum

---
+++ 2_Sum.cpp
@@ -0,0 +1,34 @@
+/*
+
+Author :- Aditya Mani Tripathi
+leetcode username - genuine_code
+email - aadiitripathii46@gmail.com
+institute - UIET CSJM UNIVERSITY KANPUR
+
+*/
+
+
+
+// code :-
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int>v;
+       int n=nums.size();
+       for(int i=0;i<n;i++)
+       {
+           for(int j=i+1;j<n;j++)
+           {
+               if(nums[i]+nums[j]==target)
+               {
+                   v.push_back(i);
+                   v.push_back(j);
+                   break;
+               }
+           }
+       } 
+       return v;
+    }
+};
